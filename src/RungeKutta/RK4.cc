b96abcca440239f23a99216fbc1138af18e83b50
aaron.sanabria <aaron.sanabria@accessnode.ucr.ac.cr>
2026-05-05 11:59:17 -0600

    feat(rk4_readfield_twoops): update method arguments and modify stages so account for explicit time evolution

--- src/RungeKutta/RK4.cc
+++ src/RungeKutta/RK4.cc
@@ -10,6 +10,9 @@ RK4::RK4(MKL_INT dim)
   Rho0c1_.resize(dim_, 0.0);
   Rho0c2_.resize(dim_, 0.0);
   Rho0c3_.resize(dim_, 0.0);
+  Rv0_.resize(dim_, 0.0);
+  Rvh2_.resize(dim_, 0.0);
+  Rvh_.resize(dim_, 0.0);
 }
 
 RK4::~RK4()
@@ -58,3 +61,51 @@ void RK4::rk4_redfield(double delta_t,
   cblas_zaxpy(dim_, &h3, &k3_[0], 1, &Rho0[0], 1);
   cblas_zaxpy(dim_, &h6, &k4_[0], 1, &Rho0[0], 1);
 }
+
+// dw(t)_dt = Qw(t) + Rv(t)
+void RK4::rk4_redfield_twoops(double delta_t,
+                       MZType &w0,
+                       MZType &Q,
+                       MZType &v0,
+		       MZType &vh2,
+		       MZType &vh,
+                       MZType &R)
+{
+  cblas_zcopy(dim_, &w0[0], 1, &Rho0c1_[0], 1);
+  cblas_zcopy(dim_, &w0[0], 1, &Rho0c2_[0], 1);
+  cblas_zcopy(dim_, &w0[0], 1, &Rho0c3_[0], 1);
+  //vectors Rv(t) at times (t, t + h/2, t + h) 
+  multiply_(Rv0_, R, v0);
+  multiply_(Rvh2_, R, vh2);
+  multiply_(Rvh_, R, vh);
+  // k1 
+  std::complex<double> one(1.0, 0.0);
+  multiply_(k1_, Q, w0);
+  cblas_zaxpy(dim_, &one, &Rv0_[0], 1, &k1_[0], 1);
+  
+  // k2
+  std::complex<double> h2(delta_t / 2.0, 0.0);
+  cblas_zaxpy(dim_, &h2, &k1_[0], 1, &Rho0c1_[0], 1);
+  multiply_(k2_, Q, Rho0c1_);
+  cblas_zaxpy(dim_, &one, &Rvh2_[0], 1, &k2_[0], 1);
+
+  // k3
+  cblas_zaxpy(dim_, &h2, &k2_[0], 1, &Rho0c2_[0], 1);
+  multiply_(k3_, Q, Rho0c2_);
+  cblas_zaxpy(dim_, &one, &Rvh2_[0], 1, &k3_[0], 1);
+
+  // k4
+  std::complex<double> h(delta_t, 0.0);
+  cblas_zaxpy(dim_, &h, &k3_[0], 1, &Rho0c3_[0], 1);
+  multiply_(k4_, Q, Rho0c3_);
+  cblas_zaxpy(dim_, &one, &Rvh_[0], 1, &k4_[0], 1);
+
+  // yt = y0 + h/6 (k1 + 2k2 + 2k3 + k4)
+  std::complex<double> h6(delta_t / 6.0, 0.0);
+  std::complex<double> h3(delta_t / 3.0, 0.0);
+  cblas_zaxpy(dim_, &h6, &k1_[0], 1, &w0[0], 1);
+  cblas_zaxpy(dim_, &h3, &k2_[0], 1, &w0[0], 1);
+  cblas_zaxpy(dim_, &h3, &k3_[0], 1, &w0[0], 1);
+  cblas_zaxpy(dim_, &h6, &k4_[0], 1, &w0[0], 1);
+}
+
--- src/RungeKutta/RK4.h
+++ src/RungeKutta/RK4.h
@@ -15,6 +15,14 @@ class RK4
                       MZType &Rho0, 
                       MZType &Red);
 
+    // dw(t)_dt = Qw(t) + Rv 
+    void rk4_redfield_twoops(double delta_t,
+                             MZType &w0,
+                             MZType &Q,
+                             MZType &v0,
+			     MZType &vh2,
+			     MZType &vh,
+                             MZType &R);
   private:
     void multiply_(MZType &Res, 
                    MZType &Red, 
@@ -27,5 +35,8 @@ class RK4
     MZType Rho0c1_;
     MZType Rho0c2_;
     MZType Rho0c3_;
+    MZType Rv0_; //buffer to store Rv(t)
+    MZType Rvh2_; //buffer to store Rv(t + h/2)
+    MZType Rvh_; //buffer to store Rv(t + h)
 };
 #endif
